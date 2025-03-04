95e1fcc0e5411ebe2465b17446652eaadef25f33
devJF <juanfabianmarcal@gmail.com>
2025-03-04 18:04:10 -0300

    Refactoring connection class

--- src/br/com/senac/leiloestdsat/dao/conectaDAO.java
+++ src/br/com/senac/leiloestdsat/dao/conectaDAO.java
@@ -5,30 +5,36 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
  * @author Adm
  */
 public class conectaDAO {
-    
-    public Connection connectDB(){
-        Connection conn = null;
-        
+    private static Connection conn;
+
+    protected static Connection connectDB() {
+        conn = null;
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/uc11?user=root&password=root");
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null,
+                    "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
     }
-    
+
+    protected static void discConnectDB() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "CONNECTION ERROR: " + e.getMessage(),
+                    "ERRO",
+                    JOptionPane.WARNING_MESSAGE);
+        }
+    }
 }
